d1832d045af228579480a3e41471c749ac1c8254
Ivan Proskuriakov <snowflax@ya.ru>
2020-09-11 15:25:47 +0300

    refactor: Исправить ошибку в блоке catch.

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,15 +25,15 @@ class Main extends React.Component {
         userName: data.name,
         userDescription: data.about,
         userAvatar: data.avatar,
-      }).catch(error => console.log(error));
-    });
+      })
+    }).catch(error => console.log(error));;
 
     //Получаем список карточек по апи
     api.getCardsList().then(data => {
       this.setState({
         cards: data
-      }).catch(error => console.log(error));
-    })
+      });
+    }).catch(error => console.log(error));
   }
 
   render () {
