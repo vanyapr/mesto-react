9eb4fb1d502a316cb23a472e7f7a62ed05896ce9
Ivan Proskuriakov <snowflax@ya.ru>
2020-09-29 14:22:54 +0300

    refactor: Переработать код попапов: использовать объекты и имена полей форм для обработки значений

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -2,48 +2,33 @@ import React from 'react';
 import PopupWithForm from "./PopupWithForm";
 
 const AddPlacePopup = React.memo((props) => {
-  const [cardName, changeCardName] = React.useState('');
-  const [cardUrl, changeCardUrl] = React.useState('');
-  const [nameIsValid, changeNameValidity] = React.useState(true);
+  const [formValues, changeFormValues] = React.useState({
+    name: '',
+    link: ''
+  });
 
   function handleSubmit (event) {
     event.preventDefault();
-
-    props.onAddPlace({
-      name: cardName,
-      link: cardUrl
-    });
-
+    props.onAddPlace(formValues);
     //Сбрасываем значения после сабмита
-    changeCardName('');
-    changeCardUrl('');
+    changeFormValues({name: '', link: ''});
   }
 
   //Для валидации нам нужно для каждого инпута вызвать валидацию и отрендерить ошибку в контейнер ошибки возле этого инпута
-
-  function handleCardNameChange (event)  {
-    changeNameValidity(event.target.validity.valid);
-    console.log();
-    changeCardName(event.target.value);
-  }
-
-  function showError (event) {
-    let errorText = event.target.validationMessage;
-  }
-
-  function handleUrlChange (event)  {
-    changeCardUrl(event.target.value);
+  function handleFormChange (event)  {
+    changeFormValues({...formValues, [event.target.name]: event.target.value});
+    console.log(formValues);
   }
 
   return (
     <PopupWithForm title="Новое место" buttonText="Создать" name="place" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit}  children={
       <>
         <div className="form__input-container">
-          <input onChange={handleCardNameChange} value={cardName} type="text" name="placeName" className="form__input form__input_value_place-name" id="place-name" minLength="1" maxLength="30" placeholder="Название" aria-label="Название места" required/>
-          <span className={`form__error ${!nameIsValid && 'form__error_active'}`} id="place-name-error">{!nameIsValid && 'Название места должно быть длиннее 1 и короче 30 символов'}</span>
+          <input onChange={handleFormChange} value={formValues.name} type="text" name="name" className="form__input" id="place-name" minLength="1" maxLength="30" placeholder="Название" aria-label="Название места" required/>
+          <span className={`form__error ${'form__error_active'}`} id="place-name-error">{'Название места должно быть длиннее 1 и короче 30 символов'}</span>
         </div>
         <div className="form__input-container">
-          <input onChange={handleUrlChange} value={cardUrl} type="url" name="placeImage" className="form__input form__input_value_image" id="place-image"  placeholder="Ссылка на картинку" aria-label="Ссылка на картинку" required/>
+          <input onChange={handleFormChange} value={formValues.link} type="url" name="link" className="form__input" id="place-image"  placeholder="Ссылка на картинку" aria-label="Ссылка на картинку" required/>
           <span className="form__error" id="place-image-error"></span>
         </div>
       </>
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -3,41 +3,31 @@ import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from '../contexts/currentUserContext';
 
 function EditProfilePopup (props) {
-  const [name, changeName] = React.useState('');
-  const [description, changeDescription] = React.useState('');
+  const [formValues, changeFormValues] = React.useState({name: '', about: ''});
   const currentUser = React.useContext(CurrentUserContext);
 
-  function onNameChange (event) {
-    changeName(event.target.value);
-  }
-
-  function onDescriptionChange (event) {
-    changeDescription(event.target.value);
+  function handleFormChange (event) {
+    changeFormValues({...formValues, [event.target.name]: event.target.value});
   }
 
   function handleSubmit (event) {
     event.preventDefault();
-
-    props.onUpdateUser({
-      name,
-      about: description
-    })
+    props.onUpdateUser(formValues);
   }
 
   React.useEffect(() => {
-    changeName(currentUser.name);
-    changeDescription(currentUser.about);
+    changeFormValues({name: currentUser.name, about: currentUser.about})
   }, [currentUser])
 
   return (
     <PopupWithForm title="Редактировать профиль" buttonText="Сохранить" name="profile" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit} children={
       <>
         <div className="form__input-container">
-          <input onChange={onNameChange} defaultValue={name} type="text" name="profileName" className="form__input form__input_value_name" id="profile-name" minLength="2" maxLength="40" aria-label="Имя" required/>
+          <input onChange={handleFormChange} defaultValue={formValues.name} type="text" name="name" className="form__input" id="profile-name" minLength="2" maxLength="40" aria-label="Имя" required/>
           <span className="form__error" id="profile-name-error"></span>
         </div>
         <div className="form__input-container">
-          <input onChange={onDescriptionChange} defaultValue={description} type="text" name="profileDescription" className="form__input form__input_value_description" id="profile-description" minLength="2" maxLength="40" aria-label="Род деятельности" required/>
+          <input onChange={handleFormChange} defaultValue={formValues.about} type="text" name="about" className="form__input" id="profile-description" minLength="2" maxLength="40" aria-label="Род деятельности" required/>
           <span className="form__error" id="profile-description-error"></span>
         </div>
       </>
